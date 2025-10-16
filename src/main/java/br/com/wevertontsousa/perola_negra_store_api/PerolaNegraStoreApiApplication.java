2b62c4723bfd0f0e0c7383346dfb13b6ee8d5fba
Weverton <weverton.tsousa@hotmail.com>
2025-10-16 15:34:41 +0000

    🎉 feat(app-setup): adiciona app base gerado pelo Spring Initializr

---
+++ src/main/java/br/com/wevertontsousa/perola_negra_store_api/PerolaNegraStoreApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.wevertontsousa.perola_negra_store_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerolaNegraStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerolaNegraStoreApiApplication.class, args);
+	}
+
+}
